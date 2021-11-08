14bfc4df7bfe1f68fdfae18cf473be8d69f9cf69
Thomas LIMA <thomas.lima@viacesi.fr>
2021-11-08 20:00:08 +0100

    prototype

--- process.js
+++ process.js
@@ -1,26 +1,20 @@
-function submit(form) {
-    var TestVar = form.name.value;
-    alert ("You typed: " + TestVar);
-}
-
-// this is the id of the form
-var frm = $('#idForm');
-
-    frm.submit(function (e) {
-
-        e.preventDefault();
-
-        $.ajax({
-            type: frm.attr('name'),
-            url: frm.attr('request'),
-            data: frm.serialize(),
-            success: function (data) {
-                console.log('Submission was successful.');
-                console.log(data);
-            },
-            error: function (data) {
-                console.log('An error occurred.');
-                console.log(data);
-            },
-        });
+$(document).ready(function () {
+    $("form").submit(function (event) {
+      var formData = {
+        name: $("#name").val(),
+        email: $("#request").val(),
+      };
+  
+      $.ajax({
+        type: "POST",
+        url: "save.php",
+        data: formData,
+        dataType: "json",
+        encode: true,
+      }).done(function (data) {
+        console.log(data);
+      });
+  
+      event.preventDefault();
     });
+  });
--- save.php
+++ save.php
@@ -1,6 +1,23 @@
 <?php
-$name = $_POST['name'];
-$request = $_POST['request'];
 
-print_r($name, $request);
-?>
+$errors = [];
+$data = [];
+
+if (empty($_POST['name'])) {
+    $errors['name'] = 'Name is required.';
+}
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Email is required.';
+}
+
+
+if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors'] = $errors;
+} else {
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+}
+
+echo json_encode($data);
