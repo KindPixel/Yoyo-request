b27ba135802d6a7db6f9ef0c6852113f36dd60fa
KindPixel <thomas.lima@viacesi.fr>
2021-11-17 13:45:46 +0100

    navbar + lib + fontawesome

---
+++ htmldisplay/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Home page</title>
+</head>
+
+<?php include("navbar.php") ?>
+
+<body>
+
+</body>
+
+
+</html>
+
---
+++ htmldisplay/navbar.php
@@ -0,0 +1,48 @@
+<!-- FontAwesome -->
+<link href="../resources/fontawesome-free-5.15.4-web/css/all.css" rel="stylesheet">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- My css -->
+<link rel="stylesheet" href="../style/style.css">
+
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark py-3">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">YOYO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample03">
+            <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="askuryoyo.html">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Disabled</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt"></i></a>
+                </li>
+            </ul>
+
+            
+            
+        </div>
+    </div>
+</nav>
+
+
