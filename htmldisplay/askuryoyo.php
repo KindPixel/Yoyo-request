057b4546da2eecddec51857ef0436d37f3359f4a
KindPixel <thomas.lima@viacesi.fr>
2021-11-22 14:50:14 +0100

    ask ur yoyo init;

--- htmldisplay/askuryoyo.php
+++ htmldisplay/askuryoyo.php
@@ -1,13 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <title>Ask ur yoyo</title>
+</head>
+
 <body>
     <?php include('./navbar.php');?>
 
-
-<form>
-    <div class="form-group asking-box">
-        <label for="exampleInputEmail1">Ur requested movie</label>
-        <input type="" class="form-control" id="request">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
+<div class="asking-box">
+    <form class="form">
+        <div class="form-group">
+            <h2>Ur requested movie</h2>
+            <input type="" class="form-control" id="request" placeholder="example : Iron man 2009">
+        </div>
+        <br>
+        <input type="submit" class="btn btn-primary">submit</input>
+    </form>
+</div>
 </body>
